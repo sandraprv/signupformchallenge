9fcf21578605ac57336ea894c8484ff1ebb79531
Sandra Périvier <sandraperivier@MacBook-Air-de-Sandra.home>
2020-11-09 18:05:12 +0100

    add js and css/html update

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
+const form = document.getElementById("form");
+const inputs = document.getElementsByTagName("input");
+var arr = [...inputs];
 
+
+form.addEventListener("submit", (event) => {
+
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i].value === "") {
+      arr[i].classList.add("error");
+      arr[i].nextElementSibling.classList.add("is-visible");
+    } else {
+      arr[i].classList.remove("error");
+      arr[i].nextElementSibling.classList.remove("is-visible");
+    }
+  }
+  event.preventDefault();
+
+});
